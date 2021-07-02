5eecbd470908d0e99baa863a76f4d0119fb9ad27
vang1ong7ang <vang1ong7ang@outlook.com>
2021-07-02 02:05:29 +0000

    dev

--- test/test_permutation.py
+++ test/test_permutation.py
@@ -1,4 +1,4 @@
-from pygrouptheory import Permutation
+from pygrouptheory.permutation import Permutation
 
 class TestPermutation:
         
@@ -6,13 +6,3 @@ class TestPermutation:
 
 
 
-
-if __name__ == "__main__":
-    p1 = Permutation({1:3, 2:2, 3:1})
-    p2 = Permutation({1:2, 2:3, 3:1})
-    print((p1*p2).to_matrix())
-    print(p1.to_cycles())
-    print(p2.to_cycles())
-    print((p1*p2).to_cycles())
-    p3 = Permutation({1:2,2:3,3:4,4:1})
-    print(p3.to_cycles())
